baebabd31def59106524138f264e22a1848a4620
dieg0MF <65663780+dieg0MF@users.noreply.github.com>
2020-10-22 09:24:17 -0300

    Updates

---
+++ precache-manifest.404d37af55f043cb0ccfc8d3936a6c78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77393b23e25d4f7308de54514dfa1e2d",
+    "url": "/storePoints/index.html"
+  },
+  {
+    "revision": "7c804629b26acdc3aa68",
+    "url": "/storePoints/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c10d8b21a707ef581da7",
+    "url": "/storePoints/static/js/2.9947c9bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/storePoints/static/js/2.9947c9bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c804629b26acdc3aa68",
+    "url": "/storePoints/static/js/main.76aca07f.chunk.js"
+  },
+  {
+    "revision": "a836cf7afe90491aad85",
+    "url": "/storePoints/static/js/runtime-main.76d47368.js"
+  }
+]);
